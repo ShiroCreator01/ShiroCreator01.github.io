5d1283457e027d453e0c7315011276b163a61636
Shiro Creator <72450395+ShiroCreator01@users.noreply.github.com>
2021-02-10 12:59:39 +0700

    Update content.js

--- knowledge/cpp-win32/content.js
+++ knowledge/cpp-win32/content.js
@@ -12,6 +12,40 @@ HINSTANCE hInst = NULL;
 LPVOID lpParam = NULL;
 HWND button1 = CreateWindowEx(dwExStyle, clazz, text, dwStyle, left, top, width, height, parent, id, hInst, lpParam);
 `;
+var c_control_checkbox = `DWORD dwExStyle = 0;
+PCTSTR clazz = _T("Button");
+PCTSTR text = _T("Check 1");
+DWORD dwStyle = WS_CHILD | WS_VISIBLE | BS_NOTIFY | BS_AUTOCHECKBOX;
+int left = 0;
+int top = 0;
+int width = 100;
+int height = 32;
+HWND parent = hWnd;
+HMENU id = NULL;
+HINSTANCE hInst = NULL;
+LPVOID lpParam = NULL;
+HWND chkbox1 = CreateWindowEx(dwExStyle, clazz, text, dwStyle, left, top, width, height, parent, id, hInst, lpParam);
+`;
+var c_control_combobox = `DWORD dwExStyle = 0;
+PCTSTR clazz = _T("ComboBox");
+PCTSTR text = _T("");
+DWORD dwStyle = WS_CHILD | WS_VISIBLE | BS_NOTIFY | CBS_DROPDOWN | WS_VSCROLL;
+int left = 0;
+int top = 0;
+int width = 100;
+int height = 100;
+HWND parent = hWnd;
+HMENU id = NULL;
+HINSTANCE hInst = NULL;
+LPVOID lpParam = NULL;
+HWND combobox1 = CreateWindowEx(dwExStyle, clazz, text, dwStyle, left, top, width, height, parent, id, hInst, lpParam);
+SendMessage(combobox1, CB_ADDSTRING, 0, (LPARAM) (new string("item 1"))->c_str());
+SendMessage(combobox1, CB_ADDSTRING, 0, (LPARAM) (new string("item 2"))->c_str());
+SendMessage(combobox1, CB_ADDSTRING, 0, (LPARAM) (new string("item 3"))->c_str());
+SendMessage(combobox1, CB_ADDSTRING, 0, (LPARAM) (new string("item 4"))->c_str());
+SendMessage(combobox1, CB_ADDSTRING, 0, (LPARAM) (new string("item 5"))->c_str());
+SendMessage(combobox1, CB_SETCURSEL, 0, 0);
+`;
 var c_control_listbox = `DWORD dwExStyle = 0;
 PCTSTR clazz = _T("ListBox");
 PCTSTR text = _T("");
@@ -91,13 +125,19 @@ function ChangeTitle(id,title){
 function CodeBox(_id, _div){
     ClearDiv(_div);
     switch(_id){
-        case `Template1`:
+        case 'Template1':
             CPre(t_1,_div);
             break;
         case 'Button':
             CPre(c_control_button,_div);
             break;
-        case `ListBox`:
+        case 'CheckBox':
+            CPre(c_control_checkbox,_div);
+            break;
+        case 'ComboBox':
+            CPre(c_control_combobox,_div);
+            break;
+        case 'ListBox':
             CPre(c_control_listbox,_div);
             break;
         case 'PasswordBox':
