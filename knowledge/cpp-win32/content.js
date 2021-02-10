58bf5dc02b6a621fb28bc2b101ab023e8adf30ba
Shiro Creator <72450395+ShiroCreator01@users.noreply.github.com>
2021-02-10 12:34:34 +0700

    Update content.js

--- knowledge/cpp-win32/content.js
+++ knowledge/cpp-win32/content.js
@@ -12,6 +12,20 @@ HINSTANCE hInst = NULL;
 LPVOID lpParam = NULL;
 HWND button1 = CreateWindowEx(dwExStyle, clazz, text, dwStyle, left, top, width, height, parent, id, hInst, lpParam);
 `;
+var c_control_textbox = `DWORD dwExStyle = 0;
+PCTSTR clazz = _T("Edit");
+PCTSTR text = _T("Text Here");
+DWORD dwStyle = WS_CHILD | WS_VISIBLE | BS_NOTIFY;
+int left = 0;
+int top = 0;
+int width = 100;
+int height = 32;
+HWND parent = hWnd;
+HMENU id = NULL;
+HINSTANCE hInst = NULL;
+LPVOID lpParam = NULL;
+HWND txtbox1 = CreateWindowEx(dwExStyle, clazz, text, dwStyle, left, top, width, height, parent, id, hInst, lpParam);
+`;
 
 function CElement(_element, _content, _div){
     var x = document.createElement(_element);
@@ -41,6 +55,9 @@ function CodeBox(_id, _div){
     switch(_id){
         case 'Button':
             CPre(c_control_button,_div);
+            break;
+        case 'TextBox':
+            CPre(c_control_textbox,_div);
             break;
         default:
             break;
