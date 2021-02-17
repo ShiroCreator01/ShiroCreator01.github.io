17e387574e545a7352c5043f0878670c0ef969b7
Shiro Creator <72450395+ShiroCreator01@users.noreply.github.com>
2021-02-17 15:47:52 +0700

    Update

--- knowledge/javascript/content.js
+++ knowledge/javascript/content.js
@@ -11,21 +11,22 @@ var c_Hexadecimal_To_Decimal = `parseInt(value,16);`;
 var c_Hexadecimal_To_Binary = `parseInt(value,16).toString(2);`;
 var c_Hexadecimal_To_Octal = `parseInt(value,16).toString(8);`;
 
-function ShowInput(_from,_to){
+function ShowInput(_from,_to,_content){
     Clear(`c_title`);
     Clear(`box`);
-    H('c_title','',_from + ' - To ' + _to,3);;
+    H('c_title','',_from + ' - To ' + _to,3);
+    Pre('box','',_content,'code');
 }
 
-function ShowDtoB(){ShowInput('Decimal','Binary');}
-function ShowDtoO(){ShowInput('Decimal','Octal');}
-function ShowDtoH(){ShowInput('Decimal','Hexadecimal');}
-function ShowBtoD(){ShowInput('Binary','Decimal');}
-function ShowBtoO(){ShowInput('Binary','Octal');}
-function ShowBtoH(){ShowInput('Binary','Hexadecimal');}
-function ShowOtoD(){ShowInput('Octal','Decimal');}
-function ShowOtoB(){ShowInput('Octal','Binary');}
-function ShowOtoH(){ShowInput('Octal','Hexadecimal');}
-function ShowHtoD(){ShowInput('Hexadecimal','Decimal');}
-function ShowHtoB(){ShowInput('Hexadecimal','Binary');}
-function ShowHtoO(){ShowInput('Hexadecimal','Octal');}
+function ShowDtoB(){ShowInput('Decimal','Binary',c_Decimal_To_Binary);}
+function ShowDtoO(){ShowInput('Decimal','Octal',c_Decimal_To_Octal);}
+function ShowDtoH(){ShowInput('Decimal','Hexadecimal',c_Decimal_To_Hexadecimal);}
+function ShowBtoD(){ShowInput('Binary','Decimal',c_Binary_To_Decimal);}
+function ShowBtoO(){ShowInput('Binary','Octal',c_Binary_To_Octal);}
+function ShowBtoH(){ShowInput('Binary','Hexadecimal',c_Binary_To_Hexadecimal);}
+function ShowOtoD(){ShowInput('Octal','Decimal',c_Octal_To_Decimal);}
+function ShowOtoB(){ShowInput('Octal','Binary',c_Octal_To_Binary);}
+function ShowOtoH(){ShowInput('Octal','Hexadecimal',c_Octal_To_Hexadecimal);}
+function ShowHtoD(){ShowInput('Hexadecimal','Decimal',c_Hexadecimal_To_Decimal);}
+function ShowHtoB(){ShowInput('Hexadecimal','Binary',c_Hexadecimal_To_Binary);}
+function ShowHtoO(){ShowInput('Hexadecimal','Octal',c_Hexadecimal_To_Octal);}
